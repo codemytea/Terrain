fdf4a825633ed26f09587c9784ec5de94a3956e2
Jacqueline Dobreva-Skevington <jacqueline.dobrevaskevington@t4connex.com>
2023-12-29 13:22:39 +0000

    more formatting

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ import * as THREE from 'three';
 import { FirstPersonControls } from 'three/addons/controls/FirstPersonControls.js';
 import { ImprovedNoise } from 'three/addons/math/ImprovedNoise.js';
 
-let container, stats;
 let camera, controls, scene, renderer;
 let mesh, texture;
 
@@ -15,25 +14,21 @@ init();
 animate();
 
 function init() {
-
     camera = new THREE.PerspectiveCamera(60, width / height, 1, 10000);
+    camera.position.set(100, 800, - 800);
+    camera.lookAt(- 100, 810, - 800);
 
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0x87CEEB);
     scene.fog = new THREE.FogExp2(0xF7E7D9, 0.0025);
 
-    const data = generateHeight(worldWidth, worldDepth);
-
-    camera.position.set(100, 800, - 800);
-    camera.lookAt(- 100, 810, - 800);
-
     const geometry = new THREE.PlaneGeometry(10000, 10000, worldWidth - 1, worldDepth - 1);
     geometry.rotateX(- Math.PI / 2);
 
     const vertices = geometry.attributes.position.array;
-
-    for (let i = 0, j = 0, l = vertices.length; i < l; i ++, j += 3) {
-        vertices[j + 1] = data[i] * 10;
+    const data = generateHeight(worldWidth, worldDepth);
+    for (let i = 0; i < vertices.length; i ++) {
+        vertices[i*3 + 1] = data[i] * 10;
     }
 
     texture = new THREE.CanvasTexture(generateTexture(data, worldWidth, worldDepth));
@@ -41,7 +36,7 @@ function init() {
     texture.wrapT = THREE.ClampToEdgeWrapping;
     texture.colorSpace = THREE.SRGBColorSpace;
 
-    mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ map: texture }));
+    mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ map: texture, color:0x964B00 }));
     scene.add(mesh);
 
     renderer = new THREE.WebGLRenderer();
@@ -52,32 +47,25 @@ function init() {
     controls.movementSpeed = 150;
     controls.lookSpeed = 0.1;
 
+    window.addEventListener('resize', _ => {
+        width = window.innerWidth;
+        height = window.innerHeight;
 
-    window.addEventListener('resize', onWindowResize);
+        camera.aspect = width / height;
+        camera.updateProjectionMatrix();
 
+        renderer.setSize(width, height);
+        controls.handleResize();
+    });
 }
 
-function onWindowResize() {
-    width = window.innerWidth;
-    height = window.innerHeight;
-
-    camera.aspect = width / height;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize(width, height);
-    controls.handleResize();
-}
 
 function generateHeight(width, height) {
-
-    let seed = Math.PI / 4;
-    window.Math.random = function () {
-        const x = Math.sin(seed ++) * 10000;
-        return x - Math.floor(x);
-    };
-
-    const size = width * height, data = new Uint8Array(size);
-    const perlin = new ImprovedNoise(), z = Math.random() * 100;
+    const size = width * height
+    const data = new Uint8Array(size);
+    const perlin = new ImprovedNoise()
+    const temp = Math.sin(Math.PI/4) * 10000
+    const z = (temp - Math.floor(temp)) * 100;
 
     let quality = 1;
 
@@ -90,13 +78,9 @@ function generateHeight(width, height) {
     }
 
     return data;
-
 }
 
 function generateTexture(data, width, height) {
-
-    let context, image, imageData, shade;
-
     const vector3 = new THREE.Vector3(0, 0, 0);
 
     const sun = new THREE.Vector3(1, 1, 1);
@@ -106,66 +90,34 @@ function generateTexture(data, width, height) {
     canvas.width = width;
     canvas.height = height;
 
-    context = canvas.getContext('2d');
+    let context = canvas.getContext('2d');
     context.fillStyle = '#000';
     context.fillRect(0, 0, width, height);
 
-    image = context.getImageData(0, 0, canvas.width, canvas.height);
-    imageData = image.data;
+    let image = context.getImageData(0, 0, canvas.width, canvas.height);
+    let imageData = image.data;
 
-    for (let i = 0, j = 0, l = imageData.length; i < l; i += 4, j ++) {
-
-        vector3.x = data[j - 2] - data[j + 2];
+    for (let i = 0; i < imageData.length; i ++) {
+        vector3.x = data[i - 2] - data[i + 2];
         vector3.y = 2;
-        vector3.z = data[j - width * 2] - data[j + width * 2];
+        vector3.z = data[i - width * 2] - data[i + width * 2];
         vector3.normalize();
 
-        shade = vector3.dot(sun);
+        const shade = vector3.dot(sun);
 
-        imageData[i] = (96 + shade * 128) * (0.5 + data[j] * 0.007);
-        imageData[i + 1] = (32 + shade * 96) * (0.5 + data[j] * 0.007);
-        imageData[i + 2] = (shade * 96) * (0.5 + data[j] * 0.007);
-
+        imageData[i*4] = (96 + shade * 128) * (0.5 + data[i] * 0.007);
+        imageData[i*4 + 1] = (32 + shade * 96) * (0.5 + data[i] * 0.007);
+        imageData[i*4 + 2] = (shade * 96) * (0.5 + data[i] * 0.007);
     }
 
     context.putImageData(image, 0, 0);
 
-    // Scaled 4x
-
-    const canvasScaled = document.createElement('canvas');
-    canvasScaled.width = width * 4;
-    canvasScaled.height = height * 4;
-
-    context = canvasScaled.getContext('2d');
-    context.scale(4, 4);
-    context.drawImage(canvas, 0, 0);
-
-    image = context.getImageData(0, 0, canvasScaled.width, canvasScaled.height);
-    imageData = image.data;
-
-    for (let i = 0, l = imageData.length; i < l; i += 4) {
-
-        const v = ~ ~ (Math.random() * 5);
-
-        imageData[i] += v;
-        imageData[i + 1] += v;
-        imageData[i + 2] += v;
-
-    }
-
-    context.putImageData(image, 0, 0);
-
-    return canvasScaled;
+    return canvas;
 }
 
 
 function animate() {
     requestAnimationFrame(animate);
-    render();
-}
-
-
-function render() {
     controls.update(clock.getDelta());
     renderer.render(scene, camera);
 }
